78fff2d5f7e44d571af1c6aea3c4c5e4629dd838
2kay <shadrackkimele@gmail.com>
2026-09-17 17:51:04 +0300

    Add logic.dart

---
+++ logic.dart
@@ -0,0 +1,12 @@
+void main(){
+  var number=10;
+  if(number==3){
+    print("the number is 3");
+  }
+  else if(number==10){
+    print("the number is not 3");
+  }
+  else{
+    print("the number is not 10 or 5 it is $number");
+  }
+}
